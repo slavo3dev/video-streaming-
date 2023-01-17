b23736acfa5e5259937c15967fddbf2598bc515b
Nick <92617254+nikolaklenje@users.noreply.github.com>
2023-01-17 17:06:19 -0500

    Added Back Button on Video Page. (#5)
    Change styles for buttons on mobile view.
    Set-up video-player parameters for video-preview play.

--- lib/videos.ts
+++ lib/videos.ts
@@ -1,12 +1,12 @@
 import data from "data/data-videos.json"
 
-interface ObjectVideoType { 
-    creatorOne: {
-       items: []   
+interface ObjectVideoType {
+  creatorOne: {
+    items: []
   }
 }
-export const getVideos = ( creator: string) => {
-  
+export const getVideos = (creator: string) => {
+
   let creatorType = creator ? creator : "creatorFour"
   console.log("Creator: ", creator)
   return data[creatorType as keyof ObjectVideoType].items.map(
@@ -17,7 +17,7 @@ export const getVideos = ( creator: string) => {
       //maping through data in order to acces same values for every object in array
       return {
         imgUrl: item.snippet.thumbnails.high.url,
-        tittle: item.snippet.title,
+        title: item.snippet.title,
         id: item.id.videoId || null,
       }
     },
--- src/pages/video/[videoId].tsx
+++ src/pages/video/[videoId].tsx
@@ -5,14 +5,18 @@ const Video = () => {
   const router = useRouter();
   console.log({ router });
   return (
-    <div className="w-full h-screen bg-blue-600" onClick={() => router.push("/")}>
+    <div className="w-full h-screen p-6 px-10 bg-blue-600">
+      <div className="bg-blue-500 hover:bg-blue-700 text-white font-bold flex space-x-2 justify-center py-2 px-4 rounded-full w-24 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out hover:shadow-lg focus:bg-blue-700 focus:shadow-lg" onClick={() => router.push("/")}>BACK</div>
       <div className="absolute player left-10 right-10 m-0-auto w-11/12 self-center bottom-10 top-24 outline-0 rounded-xl ">
         <iframe
+          sandbox="allow-forms allow-scripts allow-pointer-lock allow-same-origin allow-top-navigation"
           id="ytplayer"
           width="100%"
           height="100%"
           allowFullScreen
-          src={`https://www.youtube.com/embed/${router.query.videoId}?autoplay=0&origin=http://example.com&controls=1&rel=0`}
+          allow="autoplay"
+
+          src={`https://www.youtube.com/embed/${router.query.videoId}?origin=http://example.com&controls=0&rel=0&controls=0&disablekb=1&autoplay=1&modestbranding=1&start=10&end=23`}
         ></iframe>
       </div>
     </div>
