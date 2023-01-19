be1c213fe750d2cfdd6e7140d2067dab82bf144a
nikolaklenje <nikola@prototypenext.com>
2023-01-19 11:25:01 -0500

    Add Creator short info with Subscribe button. Set style for desktop and mobile view.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,2 @@
-import { Footer, Header, Login, Card, CardSection, Spinner } from "src/components/Layout/index"
-export { Footer, Header, Login, Card, CardSection, Spinner }
+import { Footer, Header, Login, Card, CardSection, Spinner, Profile } from "src/components/Layout/index"
+export { Footer, Header, Login, Card, CardSection, Spinner, Profile }
