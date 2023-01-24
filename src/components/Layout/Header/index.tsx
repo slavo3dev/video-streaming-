045d064df49f6060f87d431b5662ad22b671ba84
nikolaklenje <nikola@prototypenext.com>
2023-01-24 12:21:57 -0500

    Mucenje

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -5,6 +5,7 @@ import { CreateUser } from "lib//userTag"
 import { LogoutFunc } from "lib/loginFunc"
 import Image from "next/image"
 import icon from "public/monster.png"
+import { SearchBar } from "./search"
 
 export const Header: FC = () => {
 
@@ -14,49 +15,55 @@ export const Header: FC = () => {
       <div className="mobile-view text-white">
         <CreateUser />
       </div>
-      <div className="flex items-center py-4 mx-auto justify-between w-full"> 
+      <div className="flex items-center py-4 mx-auto justify-between w-full">
         <nav className="navbar navbar-expand-lg shadow-md py-2 relative flex items-center w-full">
           <div className="w-full flex flex-wrap items-center justify-between text-grey">
-  <div className="block ml-0 pr-2 lg:px-2 py-2 desktop-view"><Image width={10} height={10} className="w-10 h-10 mr-2 rounded-full" src={icon} alt={""}></Image></div>
-<div>
-            <div className="desktop-view flex justify-between mx-auto">
-                 
-              <ul className="navbar-nav mr-auto flex lflex-row">
-                
-                <li className="nav-item">
-                  <Link
-                    className="nav-link block pr-2 lg:px-2 py-2  transition duration-150 ease-in-out"
-                    href="/"
-                    data-mdb-ripple="true"
-                    data-mdb-ripple-color="light">
-                    Home
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link
-                    className="nav-link block pr-2 lg:px-2 py-2  "
-                    href="#!"
-                    data-mdb-ripple="true"
-                    data-mdb-ripple-color="light">
-                    Creator
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link
-                    className="nav-link block pr-2 lg:px-2 py-2 "
-                    href="/setings"
-                    data-mdb-ripple="true"
-                    data-mdb-ripple-color="light">
-                    Setings
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <span className="nav-link block pr-2 lg:px-2 py-2" >
-                  <LogoutFunc /></span>
-                </li>
-              </ul>
+            <div className="block ml-0 pr-2 lg:px-2 py-2 desktop-view">
+              <Link href="/"
+              ><Image width={10} height={10} className="w-10 h-10 mr-2 rounded-full" src={icon} alt={""}></Image>
+              </Link>
+              </div>
+            <div>
+              <div className="desktop-view flex justify-between mx-auto">
+
+                <ul className="navbar-nav mr-auto flex lflex-row">
+
+                  <li className="nav-item">
+                    <Link
+                      className="nav-link block pr-2 lg:px-2 py-2  transition duration-150 ease-in-out"
+                      href="/"
+                      data-mdb-ripple="true"
+                      data-mdb-ripple-color="light">
+                      Home
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link
+                      className="nav-link block pr-2 lg:px-2 py-2  "
+                      href="/creator"
+                      data-mdb-ripple="true"
+                      data-mdb-ripple-color="light">
+                      Creator
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link
+                      className="nav-link block pr-2 lg:px-2 py-2 "
+                      href="/setings"
+                      data-mdb-ripple="true"
+                      data-mdb-ripple-color="light">
+                      Setings
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <span className="nav-link block pr-2 lg:px-2 py-2" >
+                      <LogoutFunc /></span>
+                  </li>
+                  
+                </ul>
               </div>
             </div>
+            <SearchBar/>
             <div className="desktop-view text-sky-500" >
               <CreateUser />
             </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,18 @@
 import { magic } from "lib/magic-client";
 import { useEffect, useState } from "react";
-import { CardSection } from "src/components/index";
+import { Card, CardSection } from "src/components/index";
 import { Login } from "src/components/index";
 import { Header } from "src/components/index"
 import { Footer } from "src/components/index"
 import { Spinner } from "src/components/index";
+import { getVideos } from "lib/videos";
+import Link from "next/link";
 
 export default function HomePage() {
 
+   let creatorData1 = getVideos("creatorThree");
+   let creatorData2 = getVideos("creatorFour");
+   let creatorData3 = getVideos("creatorTwo");
   const [auth, setAuth] = useState<string | boolean>("loading")
 
   useEffect(() => {
@@ -21,15 +26,47 @@ export default function HomePage() {
     }
     handleLoggedIn();
   }, [auth])
+ 
+   let videoSection1= (
+<div className="overflow-x-scroll w-screen flex flex-row">
+          {creatorData1.map((video: {
+            id: string; imgUrl: string;
+          }, idx: {}) => {
+            return <Link href={`/video/${video.id}`} key={video.id + Math.random()}><Card id={idx} key={video.id} imgUrl={video.imgUrl} /></Link>;
+          })}
+        </div>
+   )
 
+   let videoSection2= (
+<div className="overflow-x-scroll w-screen flex flex-row">
+          {creatorData2.map((video: {
+            id: string; imgUrl: string;
+          }, idx: {}) => {
+            return <Link href={`/video/${video.id}`} key={video.id + Math.random()}><Card id={idx} key={video.id} imgUrl={video.imgUrl} /></Link>;
+          })}
+        </div>
+   )
+
+   let videoSection3= (
+<div className="overflow-x-scroll w-screen flex flex-row">
+          {creatorData3.map((video: {
+            id: string; imgUrl: string;
+          }, idx: {}) => {
+            return <Link href={`/video/${video.id}`} key={video.id + Math.random()}><Card id={idx} key={video.id} imgUrl={video.imgUrl} /></Link>;
+          })}
+        </div>
+   )
   let homePage = (
     <div className="flex flex-col min-h-screen w-full">
-      <div className="lg:absolute lg:inset-y-0 " >
+      <div className="lg:absolute lg:inset-y-0">
       </div>
       <Header />
-      <CardSection />
-      <CardSection />
-      <CardSection />
+<h1 className="text-4xl  pt-3 ml-6 font-bold text-blue-500">Your Videos</h1>
+      {videoSection1}
+<h1 className="text-4xl  pt-3 ml-6 font-bold text-blue-500">New Videos</h1>
+      {videoSection2}
+<h1 className="text-4xl  pt-3 ml-6 font-bold text-blue-500">Other</h1>
+      {videoSection3}
       <Footer />
     </div>
   )
