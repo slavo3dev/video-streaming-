4398b170ee52d48b9239c781666104bdadfa2989
nikolaklenje <nikola@prototypenext.com>
2023-03-21 16:10:23 -0400

    LoginPage Style

--- src/components/Layout/Login/index.tsx
+++ src/components/Layout/Login/index.tsx
@@ -1,71 +1,81 @@
-import { FC } from "react";
-import { useState } from "react";
-import { magic } from "lib/magic-client";
-import { useRouter } from "next/router";
-import Image from "next/image";
+import { FC } from "react"
+import { useState } from "react"
+import { magic } from "lib/magic-client"
+import { useRouter } from "next/router"
+import Image from "next/image"
 export const Login: FC = () => {
-
-  const router = useRouter();
+  const router = useRouter()
   const [authData, setAuthData] = useState({ email: "", userMsg: "" })
-  const handleOnChangeEmail = (e: { target: { value: string }, preventDefault: () => void }) => {
-    e.preventDefault();
+  const handleOnChangeEmail = (e: {
+    target: { value: string }
+    preventDefault: () => void
+  }) => {
+    e.preventDefault()
     setAuthData({ email: e.target.value, userMsg: "" })
-  };
+  }
   const handleLoginWithEmail = async (e: { preventDefault: () => void }) => {
-    e.preventDefault();
+    e.preventDefault()
 
-    let emailInput = authData.email;
+    let emailInput = authData.email
     if (emailInput) {
       // log in a user by their email
       try {
         console.log(emailInput)
         await magic.auth.loginWithMagicLink({
-          email: emailInput
+          email: emailInput,
         })
         router.reload()
-      }
-      catch (error) {
+      } catch (error) {
         // Handle errors if required!
-        console.error("Something went wrong logging in", error);
+        console.error("Something went wrong logging in", error)
       }
     } else {
-      setAuthData({ email: "", userMsg: "Please enter valid email" });
+      setAuthData({ email: "", userMsg: "Please enter valid email" })
     }
-  };
+  }
   return (
-    <div className="flex flex-col min-h-screen">
-
-      <Image className="center m-0 absolute top-1/4 left-2/4" width={400} height={300}src={"/../public/FClogo.jpg"} alt={"FC Logo"}></Image>
+    <div className="flex flex-col min-h-screen ">
+      <div className="center m-0 absolute top-1/4 left-2/4">
+        <Image
+          width={280}
+          height={140}
+          src={"/../public/LogoText.png"}
+          alt={"FC Logo"}
+        ></Image>
+      </div>
       <div className="center m-0 absolute top-3/4 left-2/4">
-
-        <form className=" w-80 items-start h-64 p-8 justify-start rounded-md text-left flex flex-col  showcase-content relative">
-          <h1 className="text-white mb-5 text-2xl font-bold">Sign In</h1>
-          <div className="w-64 h-48 flex flex-col ">
+        <form className="items-start h-64  center left-2/4  justify-start rounded-md text-left flex flex-col relative">
+          <div className=" h-24 flex flex-col ">
+            <h1 className="text-slate-400  text-sm font-light">Email</h1>
             <input
-              className="email rounded-md p-2 h-12 mb-7 w-full form-info-email"
+              className="email rounded-xl drop-shadow-[0_35px_35px_rgba(0,0,0,0.10)] border border-slate-200 p-2 h-12  login-input form-info-email"
               type="email"
               placeholder="Email"
               onChange={handleOnChangeEmail}
             />
           </div>
-          <div className="btn">
+
+          <div className="btn pt-6">
+            <div>
+              <input
+                className="accent-green-500"
+                value="true"
+                type="checkbox"
+              />
+              <label className="pl-2 ">Remember me</label>
+            </div>
             <button
-              className="bg-red-600 w-64 text-white cursor-pointer font-bold rounded-md btn-primary p-2"
+              className="bg-green-600 font-normal login-button h-12 text-white cursor-pointer  rounded-md btn-primary "
               type="submit"
               onClick={handleLoginWithEmail}
             >
-              Sign in
+              Login
             </button>
             <p className="text-white">{authData.userMsg}</p>
           </div>
-          <div className="text-white">
-            <div>
-              <input value="true" type="checkbox" />
-              <label>Remember me</label>
-            </div>
-          </div>
+          <div className="text-white"></div>
         </form>
       </div>
     </div>
-  );
-};
+  )
+}
